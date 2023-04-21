d46365d688b221ab6e3164db1d04adb8c3623246
TheCipherBit <Digitalfox1155@gmail.com>
2023-04-21 15:53:22 +0545

    personal

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,8 +1,21 @@
-import "../components/experience.css";
+import { useState } from "react";
+import "./experience.css";
 import code from "../images/code.jpg";
-import output from "../images/output.png";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
+export const Detail = [
+  { id: 1, title: "test1", text: "This is test 1" },
+  { id: 2, title: "test2", text: "This is test 2" },
+  { id: 3, title: "test3", text: "This is test3" },
+  { id: 4, title: "test4", text: "This is test4" },
+];
+
 export default function Experience() {
+  const [content, setContent] = useState(Detail[0]);
+
+  function handleButtonClick(data) {
+    setContent(data);
+  }
+
   return (
     <>
       <div className="experience-container">
@@ -11,20 +24,41 @@ export default function Experience() {
             0.2<h2>Where I’ve Worked</h2>
           </p>
         </div>
-        <div className="content-container"></div>
+        <div className="content-container">
+          <div className="left-content-container">
+            <button
+              className="active"
+              onClick={() => handleButtonClick(Detail[0])}
+            >
+              Test1
+            </button>
+            <button onClick={() => handleButtonClick(Detail[1])}>
+              Test2
+            </button>
+            <button onClick={() => handleButtonClick(Detail[2])}>
+              Test3
+            </button>
+            <button onClick={() => handleButtonClick(Detail[3])}>
+              Test4
+            </button>
+          </div>
+          <div className="right-content-container" style={{ color: "white" }}>
+            <h1>{content.title}</h1>
+            <p>{content.text}</p>
+          </div>
+        </div>
       </div>
       <div className="container-03">
         <p>
           0.3<h2>Some Things I’ve Built</h2>
         </p>
-        <div className="content-03">
-          <img className="img-1" src={code} alt="code" />
-          <img className="img-2" src={output} alt="out" />
-        </div>
-        <div className="gitoo">
-          <a href="#">
-            <i className="fab fa-github"></i>
-          </a>
+        <div className="image-dis-container">
+          <div className="content-03">
+            <img className="img-1" src={code} alt="code" />
+          </div>
+          <div className="container-img-right">
+            <p>A gaming site where we can buy and sell gamming products</p>
+          </div>
         </div>
       </div>
     </>
